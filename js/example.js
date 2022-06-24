6fb4f58f7def0f504528dadcdb66bf234a78cfd7
addie1988 <john19871213@gmail.com>
2022-06-24 17:50:56 +0800

    2022.06.24.17.50

--- js/example.js
+++ js/example.js
@@ -1,10 +1,30 @@
-(function () {
-  jQuery(function () {
-    this.film_rolls || (this.film_rolls = []);
-    this.film_rolls["demo"] = new FilmRoll({
-      container: "#demo",
-      height: 560,
-    });
-    return true;
+$(document).ready(function ($) {
+  $(".carousel-normal").carousel({
+    autoplay: false,
+    infinite: true,
+    circular: false,
+    delay: 4000,
+    speed: 1000,
+    direction: "right", //top, right, bottom, left
+    easing: "linear",
+
+    pager: true, // Boolean: Show pager, true or false
+
+    nav: true, // Boolean: Show navigation, true or false,
+    navItems: 1,
+
+    prevText: "Previous", // String: Text for the "previous" button
+    nextText: "Next", // String: Text for the "next" button
+
+    render: function (item) {
+      return '<li><img src="' + item.img + '" alt="" /></li>';
+    },
+    load: null,
+    /* function(callback){},*/
+
+    mousewheel: true,
+    touch: true,
+    keyboard: true,
+    responsive: true,
   });
-}.call(this));
+});
